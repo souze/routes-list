70c3efd179e27053300808bb9d4d17a04f277cdb
Erik Karlsson <erik.karlsson.6@volvocars.com>
2022-09-08 11:44:08 +0200

    Input json works

---
+++ src/ConfirmComponent.elm
@@ -0,0 +1,76 @@
+module ConfirmComponent exposing
+    ( ButtonText(..)
+    , Description(..)
+    , Msg
+    , State
+    , TargetText(..)
+    , initialState
+    , update
+    , view
+    )
+
+import CommonView
+import Element exposing (Element)
+import Element.Input
+import Element.Region exposing (description)
+
+
+type Msg
+    = ConfirmCompTextChange String
+    | FirstButtonPressed
+
+
+type State
+    = Waiting
+    | Active String
+
+
+update : Msg -> State -> State
+update msg state =
+    case msg of
+        ConfirmCompTextChange newValue ->
+            Active newValue
+
+        FirstButtonPressed ->
+            Active ""
+
+
+type ButtonText
+    = ButtonText String
+
+
+type TargetText
+    = TargetText String
+
+
+type Description
+    = Description String
+
+
+initialState : State
+initialState =
+    Waiting
+
+
+view : State -> ButtonText -> TargetText -> Description -> (Msg -> msg) -> msg -> Element msg
+view state (ButtonText buttonText) (TargetText targetText) (Description description) eventFn confirmSuccessEvent =
+    Element.column [ Element.spacing 10, Element.width Element.fill ]
+        (case state of
+            Active inputText ->
+                [ Element.text <| description ++ "\nIf so, write \"" ++ targetText ++ "\" in the text box below"
+                , Element.Input.text []
+                    { onChange = \s -> eventFn (ConfirmCompTextChange s)
+                    , text = inputText
+                    , placeholder = Nothing
+                    , label = Element.Input.labelHidden "input to confirm"
+                    }
+                , if inputText == targetText then
+                    CommonView.buttonToSendEvent "Confirm" confirmSuccessEvent
+
+                  else
+                    Element.none
+                ]
+
+            Waiting ->
+                [ CommonView.buttonToSendEvent buttonText (eventFn FirstButtonPressed) ]
+        )
--- src/Pages/InputJson.elm
+++ src/Pages/InputJson.elm
@@ -1,8 +1,18 @@
 module Pages.InputJson exposing (Model, Msg, page)
 
+import Bridge
+import CommonView
+import ConfirmComponent
+import Element exposing (Element)
+import Element.Input
 import Gen.Params.InputJson exposing (Params)
+import Json.Decode
+import JsonRoute
+import Lamdera
+import Maybe.Extra
 import Page
 import Request
+import Route exposing (NewRouteData)
 import Shared
 import View exposing (View)
 
@@ -12,8 +22,8 @@ page shared req =
     Page.element
         { init = init
         , update = update
-        , view = view
-        , subscriptions = subscriptions
+        , view = view (shared.routes |> List.length)
+        , subscriptions = \_ -> Sub.none
         }
 
 
@@ -22,12 +32,22 @@ page shared req =
 
 
 type alias Model =
-    {}
+    { text : String
+    , statusText : Maybe String
+    , parsedRoutes : Maybe (List Route.NewRouteData)
+    , confirmState : ConfirmComponent.State
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { text = ""
+      , statusText = Nothing
+      , parsedRoutes = Nothing
+      , confirmState = ConfirmComponent.initialState
+      }
+    , Cmd.none
+    )
 
 
 
@@ -35,29 +55,96 @@ init =
 
 
 type Msg
-    = ReplaceMe
+    = TextChanged String
+    | SubmitToBackend (List NewRouteData)
+    | ConfirmComponentEvent ConfirmComponent.Msg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ReplaceMe ->
-            ( model, Cmd.none )
+        ConfirmComponentEvent event ->
+            ( { model | confirmState = model.confirmState |> ConfirmComponent.update event }
+            , Cmd.none
+            )
+
+        TextChanged newValue ->
+            ( case tryDecode newValue of
+                Ok parsedRoutes ->
+                    { text = newValue
+                    , statusText = Nothing
+                    , parsedRoutes = Just parsedRoutes
+                    , confirmState = ConfirmComponent.initialState
+                    }
+
+                Err err ->
+                    { text = newValue
+                    , statusText = Just <| Json.Decode.errorToString err
+                    , parsedRoutes = Nothing
+                    , confirmState = ConfirmComponent.initialState
+                    }
+            , Cmd.none
+            )
+
+        SubmitToBackend newRoutes ->
+            ( model
+            , Lamdera.sendToBackend <| Bridge.ToBackendResetRouteList newRoutes
+            )
 
 
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+tryDecode : String -> Result Json.Decode.Error (List NewRouteData)
+tryDecode =
+    Json.Decode.decodeString JsonRoute.decodeRouteList
 
 
 
 -- VIEW
 
 
-view : Model -> View Msg
-view model =
-    View.placeholder "InputJson"
+view : Int -> Model -> View Msg
+view routeCount model =
+    { title = "Input Json"
+    , body = viewBody routeCount model
+    }
+
+
+viewBody : Int -> Model -> Element Msg
+viewBody routeCount model =
+    CommonView.mainColumnWithToprow
+        [ viewJsonInput model.text
+        , model.statusText
+            |> Maybe.map viewJsonInputError
+            |> Maybe.withDefault Element.none
+        , viewJsonInputSubmitButton model.confirmState model.parsedRoutes routeCount
+        ]
+
+
+viewJsonInputSubmitButton : ConfirmComponent.State -> Maybe (List NewRouteData) -> Int -> Element Msg
+viewJsonInputSubmitButton confirmState newRoutes routeCount =
+    case newRoutes of
+        Just newRoutes_ ->
+            ConfirmComponent.view confirmState
+                (ConfirmComponent.ButtonText "Reset climb log")
+                (ConfirmComponent.TargetText "Confirm")
+                (ConfirmComponent.Description <| "This will replace the current " ++ String.fromInt routeCount ++ " routes with the input " ++ String.fromInt (List.length newRoutes_) ++ " routes, Are you really sure?")
+                ConfirmComponentEvent
+                (SubmitToBackend newRoutes_)
+
+        Nothing ->
+            Element.none
+
+
+viewJsonInputError : String -> Element msg
+viewJsonInputError err =
+    Element.text err
+
+
+viewJsonInput : String -> Element Msg
+viewJsonInput text =
+    Element.Input.multiline []
+        { onChange = TextChanged
+        , text = text
+        , placeholder = Nothing
+        , label = Element.Input.labelAbove [] (Element.text "Json")
+        , spellcheck = False
+        }
