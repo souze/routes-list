b25a9305417943e0e0c92c6a3fb13a4c6bd2b425
Erik Karlsson <triggger@gmail.com>
2023-05-31 11:19:10 +0200

    halfway through stats

--- src/Pages/Stats.elm
+++ src/Pages/Stats.elm
@@ -1,19 +1,21 @@
 module Pages.Stats exposing (Model, Msg, page)
 
+import Chart as C
+import Dict exposing (Dict)
+import Route
+import Chart.Attributes as CA
 import CommonView
 import Element exposing (Element)
 import Gen.Params.Stats exposing (Params)
+import Html
+import Html.Attributes
 import Page
 import Request
 import Shared
-import Time
-import View exposing (View)
-import Chart as C
-import Chart.Attributes as CA
 import Svg exposing (Svg)
 import Svg.Attributes as SvgAttr
-import Html.Attributes
-import Html
+import Time
+import View exposing (View)
 
 
 page : Shared.Model -> Request.With Params -> Page.With Model Msg
@@ -62,60 +64,48 @@ update msg model =
 view : Shared.Model -> Model -> View Msg
 view shared model =
     { title = "Route List"
-    , body = Element.html (mainChart) -- viewBody shared model
+    , body = viewBody shared model
     }
 
-main : Svg msg
-main =
-    Svg.svg
-    [ SvgAttr.width "120"
-    , SvgAttr.height "120"
-    , SvgAttr.viewBox "0 0 500 500"
-    ]
-    [ Svg.rect
-        [ SvgAttr.x "10"
-        , SvgAttr.y "10"
-        , SvgAttr.width "100"
-        , SvgAttr.height "100"
-        , SvgAttr.rx "15"
-        , SvgAttr.ry "15"
+
+viewBody : Shared.Model -> Model -> Element Msg
+viewBody shared model =
+    CommonView.mainColumn
+        [ CommonView.header
+        , viewStats shared model
         ]
-        []
-    , Svg.circle
-        [ SvgAttr.cx "50"
-        , SvgAttr.cy "50"
-        , SvgAttr.r "50"
+
+
+viewStats : Shared.Model -> Model -> Element Msg
+viewStats shared model =
+    Element.column [] [
+            gradeChart shared.routes
         ]
-        []
-    , mainChart
-    ]
 
 
+gradeChart : List Route.RouteData -> Element msg
+gradeChart routes =
+    C.chart
+        [ CA.width 100
+        , CA.height 100
+        , CA.attrs [ SvgAttr.height "100" ]
+        ]
+        [ C.xTicks []
+        , C.yTicks []
+        , C.xLabels []
+        , C.yLabels []
+        , C.xAxis []
+        , C.yAxis []
+        , C.bars [] [ C.bar identity [] ] [ 2, 4, 3 ]
+        ]
+        |> Element.html
+        |> Element.el []
 
--- viewBody : Shared.Model -> Model -> Element Msg
--- viewBody shared model =
---     CommonView.mainColumn
---         [ CommonView.header
---         , viewStats shared model
---         ]
 
-
--- viewStats : Shared.Model -> Model -> Element Msg
--- viewStats shared model =
---     Element.html main
-
-mainChart : Svg msg
-mainChart =
-  C.chart
-    [ CA.width 100
-    , CA.height 100
-    , CA.attrs [SvgAttr.height "100"]
-    ]
-    [ C.xTicks []
-    , C.yTicks []
-    , C.xLabels []
-    , C.yLabels []
-    , C.xAxis []
-    , C.yAxis []
-    , C.bars [ ] [ C.bar identity [] ]  [ 2, 4, 3 ]
-    ]
+groupByGrade : List Route.RouteData -> Dict String (List Route.RouteData)
+groupByGrade routes =
+    let
+        foldHelper = 1
+    in
+    routes
+        |> List.foldl foldHelper Dict.empty
